389d54a13a9ffaa42ca36bda55afd6da422dbaaf
DT0814 <729742011@qq.com>
2017-11-18 11:18:56 +0800

    DT

--- src/main/java/com/xawl/tourism/controller/UserController.java
+++ src/main/java/com/xawl/tourism/controller/UserController.java
@@ -3,8 +3,6 @@ package com.xawl.tourism.controller;
 
 import com.xawl.tourism.pojo.User;
 import com.xawl.tourism.service.UserService;
-import com.xawl.tourism.utils.Result;
-import com.xawl.tourism.utils.UUIDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 import java.util.List;
 
 
@@ -30,18 +27,5 @@ public class UserController {
     @RequestMapping("/findAll.action")
     public List<User> findAll(HttpServletRequest request, HttpServletResponse response) {
         return userService.findAll();
-    }
-
-    @ResponseBody
-    @RequestMapping("/addUser.action")
-    public Result addUser(User user) {
-        user.setAccount("qq729742011");
-        user.setPass("123456");
-        user.setPhone("15229265560");
-        user.setUid(UUIDUtils.createUUID());
-        user.setSigninTime(new Date());
-        user.setSex("M");
-        Result result = userService.addUser(user);
-        return result;
     }
 }
--- src/main/java/com/xawl/tourism/service/UserService.java
+++ src/main/java/com/xawl/tourism/service/UserService.java
@@ -2,7 +2,6 @@ package com.xawl.tourism.service;
 
 import com.xawl.tourism.dao.UserMapper;
 import com.xawl.tourism.pojo.User;
-import com.xawl.tourism.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +18,5 @@ public class UserService {
 
     public List<User> findAll() {
         return userMapper.findAll();
-    }
-
-    public Result addUser(User user) {
-        try {
-            userMapper.insert(user);
-            return Result.success(user);
-        } catch (Exception e) {
-            return Result.fail(405, "添加失败");
-        }
     }
 }
